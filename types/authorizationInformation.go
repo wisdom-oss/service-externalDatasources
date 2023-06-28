40ecd7643d20c9564e6a9aeb1773c379ba90b648
Jan Eike Suchard <jan@suchard.cloud>
2023-06-28 08:12:31 +0200

    ✨ adding db read/write functions for the authorizationInformation type

--- types/authorizationInformation.go
+++ types/authorizationInformation.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"fmt"
+	"github.com/jackc/pgtype"
+)
+
 type AuthorizationDataLocation string
 
 const (
@@ -7,10 +12,40 @@ const (
 	LOCATION_QUERY  AuthorizationDataLocation = "query"
 )
 
+func (loc AuthorizationDataLocation) ToString() string {
+	return fmt.Sprintf("%s", loc)
+}
+
 type AuthorizationInformation struct {
 	Location AuthorizationDataLocation
 	Key      string
 	Value    string
 }
 
-// TODO: implement conversion/parsing functions
+// DecodeBinary implements a function to allow the decoding of the AuthorizationInformation type in database situations
+func (ai AuthorizationInformation) DecodeBinary(ci *pgtype.ConnInfo, src []byte) error {
+	if src == nil {
+		return ErrNilDecoding
+	}
+	if err := (pgtype.CompositeFields{&ai.Location, &ai.Key, &ai.Value}).DecodeBinary(ci, src); err != nil {
+		return err
+	}
+	return nil
+}
+
+// EncodeBinary implements a function to allow the encoding of the AuthorizationInformation type in database situations
+func (ai AuthorizationInformation) EncodeBinary(ci *pgtype.ConnInfo, buf []byte) (newBuf []byte, err error) {
+	loc := pgtype.Text{
+		String: ai.Location.ToString(),
+		Status: pgtype.Present,
+	}
+	key := pgtype.Text{
+		String: ai.Key,
+		Status: pgtype.Present,
+	}
+	val := pgtype.Text{
+		String: ai.Value,
+		Status: pgtype.Present,
+	}
+	return (pgtype.CompositeFields{&loc, &key, &val}).EncodeBinary(ci, buf)
+}
