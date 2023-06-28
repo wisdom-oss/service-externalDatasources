d0cb384dc95c08d5b12434cfacc203c407d7e074
Jan Eike Suchard <jan@suchard.cloud>
2023-06-28 10:51:52 +0200

    ✨ adding conversion for documentation

--- types/documentation.go
+++ types/documentation.go
@@ -1,9 +1,40 @@
 package types
 
+import "github.com/jackc/pgtype"
+
 type Documentation struct {
 	Type      string
 	Location  []Tuple
 	Verbosity NoneHighRange
+	Status    pgtype.Status
 }
 
-// TODO: implement conversion/parsing functions
+func (doc *Documentation) DecodeBinary(ci *pgtype.ConnInfo, src []byte) error {
+	if src == nil {
+		*doc = Documentation{Status: pgtype.Null}
+		return nil
+	}
+
+	var fields pgtype.CompositeFields
+	fields = append(
+		fields,
+		&doc.Type,
+		&doc.Location,
+		&doc.Verbosity,
+	)
+	if err := fields.DecodeBinary(ci, src); err != nil {
+		*doc = Documentation{Status: pgtype.Null}
+		return err
+	}
+	doc.Status = pgtype.Present
+	return nil
+}
+
+func (doc *Documentation) EncodeBinary(ci *pgtype.ConnInfo, buf []byte) (newBuf []byte, err error) {
+	fields := pgtype.CompositeFields{
+		&doc.Type,
+		&doc.Location,
+		&doc.Verbosity,
+	}
+	return fields.EncodeBinary(ci, buf)
+}
