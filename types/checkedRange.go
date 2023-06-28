806f7962e452c45f8b0e8d8b42c08140b9778f5f
Jan Eike Suchard <jan@suchard.cloud>
2023-06-28 09:05:28 +0200

    ✨ adding conversion for checked range

--- types/checkedRange.go
+++ types/checkedRange.go
@@ -1,8 +1,29 @@
 package types
 
+import "github.com/jackc/pgtype"
+
 type CheckedRange struct {
 	Checked bool
-	Range   NoneHighRange
+	Range   NoneHighRangeType
 }
 
-// TODO: implement conversion/parsing functions
+// DecodeBinary implements a function to allow the decoding of the CheckedRange type in database situations
+func (cr *CheckedRange) DecodeBinary(ci *pgtype.ConnInfo, src []byte) error {
+	if src == nil {
+		*cr = CheckedRange{}
+		return nil
+	}
+	if err := (pgtype.CompositeFields{&cr.Checked, &cr.Range}).DecodeBinary(ci, src); err != nil {
+		return err
+	}
+	return nil
+}
+
+// EncodeBinary implements a function to allow the encoding of the AuthorizationInformation type in database situations
+func (cr *CheckedRange) EncodeBinary(ci *pgtype.ConnInfo, src []byte) (newBuf []byte, err error) {
+	checked := pgtype.Bool{
+		Bool:   cr.Checked,
+		Status: pgtype.Present,
+	}
+	return (pgtype.CompositeFields{&checked, &cr.Range}).EncodeBinary(ci, src)
+}
