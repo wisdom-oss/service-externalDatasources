a001f5f68a91ab98e7fc2a6e662e7330058063b8
Jan Eike Suchard <jan@suchard.cloud>
2023-07-11 21:39:29 +0200

    ✨ implemented scan interface on `dataObjectivity`

--- types/dataObjectivity.go
+++ types/dataObjectivity.go
@@ -1,7 +1,46 @@
 package types
 
+import (
+	"errors"
+	"regexp"
+	"strconv"
+)
+
 type DataObjectivity struct {
-	ConflictOfInterest bool
-	RawData            bool
-	AutomaticCapture   bool
+	ConflictOfInterest bool `json:"conflictOfInterest"`
+	RawData            bool `json:"rawData"`
+	AutomaticCapture   bool `json:"automaticCapture"`
+}
+
+func (do *DataObjectivity) Scan(src interface{}) error {
+	var rowString string
+	switch src.(type) {
+	case []byte:
+		rowString = string(src.([]byte))
+	case string:
+		rowString = src.(string)
+	default:
+		return errors.New("unsupported scan input")
+	}
+	regex := regexp.MustCompile(`^\(([tf]),([tf]),([tf])\)$`)
+	matches := regex.FindStringSubmatch(rowString)
+	if len(matches) < 4 {
+		return errors.New("not enough entries in data type")
+	}
+	var obj DataObjectivity
+	var err error
+	obj.ConflictOfInterest, err = strconv.ParseBool(matches[1])
+	if err != nil {
+		return err
+	}
+	obj.RawData, err = strconv.ParseBool(matches[2])
+	if err != nil {
+		return err
+	}
+	obj.AutomaticCapture, err = strconv.ParseBool(matches[3])
+	if err != nil {
+		return err
+	}
+	*do = obj
+	return nil
 }
