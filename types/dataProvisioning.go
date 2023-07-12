a4fb4bfc2a05adeb902171449c3b3a882d5ac5bb
Jan Eike Suchard <jan@suchard.cloud>
2023-07-12 10:57:51 +0200

    ✨ implementing new type `DataProvisioning` with Scanner interface

---
+++ types/dataProvisioning.go
@@ -0,0 +1,31 @@
+package types
+
+import (
+	"errors"
+	"regexp"
+)
+
+type DataProvisioning struct {
+	Type   string `json:"type"`
+	Format string `json:"format"`
+}
+
+func (dp *DataProvisioning) Scan(src interface{}) error {
+	var rowString string
+	switch src.(type) {
+	case []byte:
+		rowString = string(src.([]byte))
+	case string:
+		rowString = src.(string)
+	default:
+		return errors.New("unsupported scan input")
+	}
+	regex := regexp.MustCompile(`^\(([^}()[\],]+),([^{}()[\],]+)\)$`)
+	matches := regex.FindStringSubmatch(rowString)
+	if len(matches) != 3 {
+		return errors.New("unexpected count of matches")
+	}
+	dp.Type = matches[1]
+	dp.Format = matches[2]
+	return nil
+}
