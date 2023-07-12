550ec1226678172ea307da780a8ea1d65fd3ee5a
Jan Eike Suchard <jan@suchard.cloud>
2023-07-12 14:05:39 +0200

    ✨ introduced scanner on logicalConsistency

--- types/logicalConsistency.go
+++ types/logicalConsistency.go
@@ -1,7 +1,37 @@
 package types
 
+import (
+	"errors"
+	"regexp"
+	"strconv"
+)
+
 type LogicalConsistency struct {
 	Checked                  bool
 	ContradictionsExaminable bool
 	Range                    NoneHighRange
+}
+
+func (lc *LogicalConsistency) Scan(src interface{}) error {
+	var rowString string
+	switch src.(type) {
+	case []byte:
+		rowString = string(src.([]byte))
+	case string:
+		rowString = src.(string)
+	default:
+		return errors.New("unsupported scan input")
+	}
+	regex := regexp.MustCompile(`^\(([tf]),([tf]),(none|low|medium|high)\)$`)
+	matches := regex.FindStringSubmatch(rowString)
+	if len(matches) != 4 {
+		return errors.New("unexpected count of matches in regex")
+	}
+	var err error
+	lc.Checked, err = strconv.ParseBool(matches[1])
+	if err != nil {
+		return err
+	}
+	lc.ContradictionsExaminable, err = strconv.ParseBool(matches[2])
+	return nil
 }
