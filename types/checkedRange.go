562e887a08e15c44e4a29a09dfe48e99f949ee00
Jan Eike Suchard <jan@suchard.cloud>
2023-07-12 11:38:58 +0200

    ✨ implemented Scanner interface on `CheckedRange`

--- types/checkedRange.go
+++ types/checkedRange.go
@@ -1,6 +1,36 @@
 package types
 
+import (
+	"errors"
+	"regexp"
+	"strconv"
+)
+
 type CheckedRange struct {
 	Checked bool
-	Range   NoneHighRangeType
+	Range   NoneHighRange
+}
+
+func (cr *CheckedRange) Scan(src interface{}) error {
+	var rowString string
+	switch src.(type) {
+	case []byte:
+		rowString = string(src.([]byte))
+	case string:
+		rowString = src.(string)
+	default:
+		return errors.New("unsupported scan input")
+	}
+	regex := regexp.MustCompile(`^\(([tf]),(none|low|medium|high)\)$`)
+	matches := regex.FindStringSubmatch(rowString)
+	if len(matches) != 3 {
+		return errors.New("unsupported count of matches found")
+	}
+	var err error
+	cr.Checked, err = strconv.ParseBool(matches[1])
+	if err != nil {
+		return err
+	}
+	cr.Range = NoneHighRange(matches[2])
+	return nil
 }
