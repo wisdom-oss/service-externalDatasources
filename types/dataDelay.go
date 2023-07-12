79cce268fd6ed00ba2132612c40fb442fd60cc24
Jan Eike Suchard <jan@suchard.cloud>
2023-07-12 14:14:14 +0200

    ✨ adding scanner interface on dataDelay

--- types/dataDelay.go
+++ types/dataDelay.go
@@ -1,6 +1,31 @@
 package types
 
+import (
+	"errors"
+	"regexp"
+)
+
 type DataDelay struct {
-	Ingress NoneHighRange
-	Egress  NoneHighRange
+	Ingress NoneHighRange `json:"ingress"`
+	Egress  NoneHighRange `json:"egress"`
+}
+
+func (dd *DataDelay) Scan(src interface{}) error {
+	var rowString string
+	switch src.(type) {
+	case []byte:
+		rowString = string(src.([]byte))
+	case string:
+		rowString = src.(string)
+	default:
+		return errors.New("unsupported scan input")
+	}
+	regex := regexp.MustCompile(`^\((none|low|medium|high),(none|low|medium|high)\)$`)
+	matches := regex.FindStringSubmatch(rowString)
+	if len(matches) != 3 {
+		return errors.New("unexpected match count")
+	}
+	dd.Ingress = NoneHighRange(matches[1])
+	dd.Egress = NoneHighRange(matches[2])
+	return nil
 }
