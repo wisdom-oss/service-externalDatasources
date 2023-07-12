d3cccd211d2d75a88124ada2d99a42c82d99caf8
Jan Eike Suchard <jan@suchard.cloud>
2023-07-12 15:08:19 +0200

    ✨ adding dataReference to the API output

---
+++ types/dataReference.go
@@ -0,0 +1,44 @@
+package types
+
+import (
+	"errors"
+	"regexp"
+	"time"
+)
+
+type DataReference struct {
+	Topic             string       `json:"topic"`
+	LocalReference    string       `json:"localReference"`
+	TemporalReference [2]time.Time `json:"temporalReference"`
+}
+
+func (dr *DataReference) Scan(src interface{}) error {
+	var rowString string
+	switch src.(type) {
+	case []byte:
+		rowString = string(src.([]byte))
+	case string:
+		rowString = src.(string)
+	default:
+		return errors.New("unsupported scan input")
+	}
+	regex := regexp.MustCompile(`^\(([^[:cntrl:],]+),([^[:cntrl:],]+),"\[([0-9]{4}-[0-9]{2}-[0-9]{2}),([0-9]{4}-[0-9]{2}-[0-9]{2})\)"\)$`)
+	matches := regex.FindStringSubmatch(rowString)
+	if len(matches) != 5 {
+		return errors.New("unexpected count of matches")
+	}
+	var obj DataReference
+	obj.Topic = matches[1]
+	obj.LocalReference = matches[2]
+	startTime, err := time.Parse("2006-01-02", matches[3])
+	if err != nil {
+		return err
+	}
+	endTime, err := time.Parse("2006-01-02", matches[4])
+	if err != nil {
+		return err
+	}
+	obj.TemporalReference = [2]time.Time{startTime, endTime}
+	*dr = obj
+	return nil
+}
