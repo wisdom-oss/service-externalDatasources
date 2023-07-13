1ddb79298331338e91d7519302d0b9f6db5309c6
Jan Eike Suchard <jan@suchard.cloud>
2023-07-13 15:13:22 +0200

    🚑️ fixing regular expression not working when having special chars

--- types/documentation.go
+++ types/documentation.go
@@ -27,7 +27,7 @@ func (d *Documentation) Scan(src interface{}) error {
 		return errors.New("unsupported scan input")
 	}
 	var doc Documentation
-	regex := regexp.MustCompile(`^\{"\('((?:[^"']|\\'|\\")+)','((?:[^"']|\\'|\\")+)',(none|low|medium|high)\)"\}$`)
+	regex := regexp.MustCompile(`^\{"\(([^[:cntrl:],]+),([^[:cntrl:],]+),(none|low|medium|high)\)\}$`)
 	values := regex.FindStringSubmatch(rowString)[1:]
 	if len(values) < 3 {
 		return errors.New("not enough items in object")
@@ -59,31 +59,15 @@ func (d *Documentations) Scan(src interface{}) error {
 	}
 	entries := strings.Split(rowString, "\",\"")
 	var documentationElements Documentations
-	for idx, entry := range entries {
+	for _, entry := range entries {
 		var doc Documentation
-		var regex *regexp.Regexp
-		switch {
-		// there is only one documentation entry
-		case idx == 0 && len(entries) == 1:
-			regex = regexp.MustCompile(`^\{"\('((?:[^"']|\\'|\\")+)','((?:[^"']|\\'|\\")+)',(none|low|medium|high)\)"\}$`)
-			break
-		// the current parsed entry is the first one of at least two
-		case idx == 0 && len(entries) > 1:
-			regex = regexp.MustCompile(`^\{"\(((?:[^"']|\\'|\\")+),((?:[^"']|\\'|\\")+),(none|low|medium|high)\)$`)
-			break
-		// the currently parsed entry is the last one of all entries
-		case (idx + 1) == len(entries):
-			regex = regexp.MustCompile(`^\(((?:[^"']|'|\\")+),((?:[^"']|\\'|\\")+),(none|low|medium|high)\)"\}$`)
-			break
-		default:
-			regex = regexp.MustCompile(`^\(((?:[^"']|\\'|\\")+),((?:[^"']|\\'|\\")+),(none|low|medium|high)\)$`)
-		}
+		regex := regexp.MustCompile(`^{*"*\(([^[:cntrl:],]+),([^[:cntrl:],]+),(none|low|medium|high)\)"*}*$`)
 		submatches := regex.FindStringSubmatch(entry)
 		values := submatches[1:]
 		if len(values) < 3 {
 			return errors.New("not enough items in object")
 		}
-		doc.Type = strings.Trim(values[0], `\"`)
+		doc.Type = strings.Trim(values[0], ``)
 		doc.Location = values[1]
 		doc.Verbosity = enums.NoneHighRange(values[2])
 		documentationElements = append(documentationElements, doc)
