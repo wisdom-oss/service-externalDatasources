9638ec660eb8a30e04911f479cd8dd2f2b835fed
Jan Eike Suchard <jan@suchard.cloud>
2024-03-11 14:26:08 +0100

    removed additional headers from the api call

--- types/api.go
+++ types/api.go
@@ -1,16 +1,13 @@
 package types
 
-import databaseTypes "external-api-service/types/database"
-
 type API struct {
-	IsSecure          *bool                 `json:"isSecure" db:"is_secure"`
-	Host              *string               `json:"host" db:"host"`
-	Port              *uint16               `json:"port" db:"port"`
-	Path              *string               `json:"path" db:"path"`
-	AdditionalHeaders *databaseTypes.Tuples `json:"additionalHeaders"`
+	IsSecure *bool   `json:"isSecure" db:"is_secure"`
+	Host     *string `json:"host" db:"host"`
+	Port     *uint16 `json:"port" db:"port"`
+	Path     *string `json:"path" db:"path"`
 }
 
-// IsValid checks if at least the host was set in the configuration
-func (a API) IsValid() bool {
+// Valid checks if at least the host was set in the configuration
+func (a API) Valid() bool {
 	return a.Host != nil
 }
