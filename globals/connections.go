ec90e5f7c86e01c428ff3041c34cbdaf981c136f
Jan Eike Suchard <jan@suchard.cloud>
2024-03-11 10:57:06 +0100

    switching to pgxpool as db driver

--- globals/connections.go
+++ globals/connections.go
@@ -1,14 +1,15 @@
 package globals
 
 import (
-	"database/sql"
 	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // This file contains all globally shared connections (e.g., Databases)
 
 // Db contains the globally available connection to the database
-var Db *sql.DB
+var Db *pgxpool.Pool
 
 // HttpClient contains the globally used http client in the proxy switch
 var HttpClient = &http.Client{}
